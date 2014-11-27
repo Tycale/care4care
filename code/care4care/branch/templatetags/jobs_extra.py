d8716f391e6a095772561d6e0f094d3d11e8a0fb
Gérondal Thibault <gthibault5@gmail.com>
2014-11-27 16:06:49 +0100

    Hide branch on keywords args

--- code/care4care/branch/templatetags/jobs_extra.py
+++ code/care4care/branch/templatetags/jobs_extra.py
@@ -2,9 +2,9 @@ from django import template
 register = template.Library()
 
 @register.inclusion_tag('templatetags/offers.html')
-def show_offers(request, offers):
+def show_offers(request, offers, show_branch=True):
     return locals()
 
 @register.inclusion_tag('templatetags/demands.html')
-def show_demands(request, demands):
+def show_demands(request, demands, show_branch=True):
     return locals()
