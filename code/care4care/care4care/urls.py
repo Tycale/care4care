1d72b7efb2a2e656aba415d11a343bee69448857
Gérondal Thibault <gthibault5@gmail.com>
2014-11-29 15:27:32 +0100

    Auto-complete working

--- code/care4care/care4care/urls.py
+++ code/care4care/care4care/urls.py
@@ -3,11 +3,14 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
+from ajax_select import urls as ajax_select_urls
+
 urlpatterns = patterns(
 	'',
     # Examples:
     # url(r'^$', 'care4care.views.home', name='home'),
     url(r'', include('main.urls')),
+    (r'^lookups/', include(ajax_select_urls)),
     url(r'branch/', include('branch.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^i18n/', include('django.conf.urls.i18n')),
