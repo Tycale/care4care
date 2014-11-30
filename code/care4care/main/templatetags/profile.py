6dbf88d83a01797a5e58e47eed613b7177e71e64
Michael H <michael.heraly@student.uclouvain.be>
2014-11-30 15:32:05 +0100

    Add primary version of user stats (not yet functional)

--- code/care4care/main/templatetags/profile.py
+++ code/care4care/main/templatetags/profile.py
@@ -3,12 +3,17 @@ register = template.Library()
 
 @register.inclusion_tag('templatetags/favorites.html')
 def show_favorites(request, favorites):
-        return locals()
+	return locals()
 
 @register.inclusion_tag('templatetags/personal_network.html')
 def show_personal_network(request, network):
-        return locals()
+	return locals()
 
 @register.inclusion_tag('templatetags/ignored_list.html')
 def show_ignore_list(request, ignore_list):
-        return locals()
+	return locals()
+
+
+@register.inclusion_tag('templatetags/user_stats.html')
+def show_user_stats(request, user_id):
+	return locals()
