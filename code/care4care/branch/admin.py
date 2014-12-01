b54aee4c7e06da643d68474b9eeac81776cf117c
Denis Genon <denisgenon@gmail.com>
2014-12-01 18:49:07 +0100

    admin

--- code/care4care/branch/admin.py
+++ code/care4care/branch/admin.py
@@ -1,16 +1,31 @@
 from django.contrib import admin
 
-from branch.models import Demand, Offer, Comment
+from branch.models import Demand, Offer, Comment, Branch
 
 class OfferAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('category', 'get_user', 'branch', 'date',)
+    search_fields = ('get_user','branch','category','title')
+    def get_user(self, obj):
+        return obj.offer.username
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__username'
 
 class DemandAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('title','category', 'get_user', 'branch', 'date',)
+    search_fields = ('get_user','branch','category','title')
+    def get_user(self, obj):
+        return obj.receiver.username
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__username'
 
 class CommentAdmin(admin.ModelAdmin):
+    pass
+
+class BranchAdmin(admin.ModelAdmin):
     pass
 
 admin.site.register(Demand, DemandAdmin)
 admin.site.register(Offer, OfferAdmin)
 admin.site.register(Comment, CommentAdmin)
+admin.site.register(Branch, BranchAdmin)
+
--- code/care4care/main/admin.py
+++ code/care4care/main/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from main.models import User, VerifiedInformation
+from main.models import User, VerifiedInformation, EmergencyContact
 
 class CareUserAdmin(admin.ModelAdmin):
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    #list_display = ('username', 'email', 'first_name', 'last_name', 'birth_date', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'birth_date', 'is_staff')
     #list_filter = ('is_staff', 'is_superuser')
     #fieldsets = (
     #    (None, {'fields': ('email', 'password')}),
@@ -23,15 +23,29 @@ class CareUserAdmin(admin.ModelAdmin):
     #        'fields': ('email', 'first_name', 'last_name', 'birth_date', 'password1', 'password2')}
     #    ),
     #)
-    #search_fields = ('email',)
+    search_fields = ('username', 'email', 'first_name', 'last_name',)
     #ordering = ('email',)
     #filter_horizontal = ()
-    pass
-
-admin.site.register(User, CareUserAdmin)
-
+    #pass
 
 class VerifiedInformationAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_user', 'recomendation_letter_1', 'recomendation_letter_2', 'criminal_record',)
+    search_fields = ('get_user',)
+    def get_user(self, obj):
+        return obj.user.username
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__username'
+    #pass
 
+class EmergencyContactAdmin(admin.ModelAdmin):
+    list_display = ('get_user', 'order',)
+    search_fields = ('get_user',)   
+    def get_user(self, obj):
+        return obj.user.username
+    get_user.short_description = 'User'
+    get_user.admin_order_field = 'user__username'
+
+
+admin.site.register(User, CareUserAdmin)
 admin.site.register(VerifiedInformation, VerifiedInformationAdmin)
+admin.site.register(EmergencyContact, EmergencyContactAdmin)
