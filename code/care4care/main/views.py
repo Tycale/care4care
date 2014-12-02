e2b3eb808c2b3e57651fbc467cb95131122976f1
amoyaux <arnold.moyaux@student.uclouvain.be>
2014-12-02 16:03:12 +0100

    commit before merge

--- code/care4care/main/views.py
+++ code/care4care/main/views.py
@@ -572,3 +572,18 @@ def job_search_view(request):
 
     return render(request,'search/job.html', locals())
 
+### CREDIT ###
+
+@login_required
+def credits_view(request):
+    user = request.user
+    jobs = Demand.objects.filter(closed=True,donor=user).all()
+    num_jobs = len(jobs)
+    average_time_job = 0
+    for job in jobs :
+        average_time_job += job.real_time
+    if num_jobs != 0:
+        average_time_job = average_time_job/num_jobs
+
+
+    return render(request,'credits/menu.html', locals())
