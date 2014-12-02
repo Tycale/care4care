a9783153709117cc0d336d986172ece73fe96cca
Michael H <michael.heraly@student.uclouvain.be>
2014-12-02 10:31:19 +0100

    The police route is more concise now

--- code/care4care/main/views.py
+++ code/care4care/main/views.py
@@ -419,9 +419,14 @@ def similar_offers(request):
 
     return render(request, 'seek_similar_jobs/main.html', locals())
 
+
 ### Statistics ###
 
+@login_required
 def statistics(request):
+    if not request.user.is_superuser:
+        return redirect('home')
+
     # Account status color
     ACTIVE_COLOR_HEX = Statistics.ACTIVE_COLOR_HEX
     ON_HOLIDAY_COLOR_HEX = Statistics.ON_HOLIDAY_COLOR_HEX
@@ -442,59 +447,66 @@ PERMISSION_DENIED = "Permission denied. This event will be reported."
 
 @login_required
 def get_registrated_users_json(request):
-    if request.user.is_superuser:
-        return get_json_from(Statistics.get_users_registrated_json())
-    else:
+    if not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_users_registrated_json())
+
 
 @login_required
 def get_account_types_json(request):
-    if request.user.is_superuser:
-        return get_json_from(Statistics.get_account_types_json())
-    else:
+    if not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+    return get_json_from(Statistics.get_account_types_json())
+
 
 @login_required
 def get_users_status_json(request):
-    if request.user.is_superuser:
-        return get_json_from(Statistics.get_users_status_json())
-    else:
+    if not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_users_status_json())
+
 
 @login_required
 def get_job_categories_json(request):
-    if request.user.is_superuser:
-        return get_json_from(Statistics.get_job_categories_json())
-    else:
+    if not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_job_categories_json())
+
 
 @login_required
 def get_user_job_categories_json(request, user_id):
-    if request.user.id == int(user_id) or request.user.is_superuser:
-        return get_json_from(Statistics.get_user_job_categories_json(user_id))
-    else:
+    if request.user.id != int(user_id) or not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_user_job_categories_json(user_id))
+
 
 @login_required
 def get_user_job_avg_time_json(request, user_id):
-    if request.user.id == int(user_id) or request.user.is_superuser:
-        return get_json_from(Statistics.get_user_job_avg_time_json(user_id))
-    else:
+    if request.user.id != int(user_id) or not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_user_job_avg_time_json(user_id))
+
 
 @login_required
 def get_user_km_json(request, user_id):
-    if request.user.id == int(user_id) or request.user.is_superuser:
-        return get_json_from(Statistics.get_user_km_json(user_id))
-    else:
+    if request.user.id != int(user_id) or not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_user_km_json(user_id))
+
 
 @login_required
 def get_user_jobs_amount_json(request, user_id):
-    if request.user.id == int(user_id) or request.user.is_superuser:
-        return get_json_from(Statistics.get_user_jobs_amount_json(user_id))
-    else:
+    if request.user.id != int(user_id) or not request.user.is_superuser:
         return HttpResponse(PERMISSION_DENIED, status=401)
+
+    return get_json_from(Statistics.get_user_jobs_amount_json(user_id))
+
 
 ### Search ###
 @login_required
