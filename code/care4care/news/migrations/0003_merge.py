759b0d6570f8d92b383c0f37aeb57c12c9a5761a
alokhan <alokhan@users.noreply.github.com>
2014-12-03 17:33:14 +0100

    merge conflict fix

---
+++ code/care4care/news/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0002_auto_20141203_1605'),
+        ('news', '0002_auto_20141203_1619'),
+    ]
+
+    operations = [
+    ]
