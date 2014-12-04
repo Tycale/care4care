e790a5ab01047256c3f93d4b965e7593d38c9d2a
alokhan <alokhan@users.noreply.github.com>
2014-12-04 20:10:11 +0100

    social oauth work in progress

---
+++ code/care4care/care4care/adapter.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+
+from main.models import User
+
+class MyAccountAdapter(DefaultSocialAccountAdapter):
+
+    def populate_user(self, request, sociallogin, data):
+        sociallogin.account.user.is_active = True
+
+        username = data['first_name'] + data['last_name']
+        if User.objects.filter(username=username).exists():
+            base_username = username
+            count = 1
+            while User.objects.filter(username=username).exists():
+                username = base_username + str(count)
+                count += 1
+
+        sociallogin.account.user.first_name = data.get('first_name')
+        sociallogin.account.user.last_name = data.get('last_name')
+        sociallogin.account.user.email = data.get('email')
+        sociallogin.account.user.name = data.get('name')
+        sociallogin.account.user.username = username
+        sociallogin.account.user.save()
+        return sociallogin.account.user
