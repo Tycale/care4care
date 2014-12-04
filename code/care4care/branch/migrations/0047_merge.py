fe1082e562e18900c3dc3b659dc21c99648603a3
alokhan <mail@aloispaulus.be>
2014-12-04 20:22:12 +0100

    merge migrations

---
+++ code/care4care/branch/migrations/0047_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('branch', '0046_auto_20141204_1818'),
+        ('branch', '0045_auto_20141204_1532'),
+    ]
+
+    operations = [
+    ]
