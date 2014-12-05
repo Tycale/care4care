ecfb4dbe6ac1bc3c170440c240049600920fbe2c
Gérondal Thibault <gthibault5@gmail.com>
2014-12-05 17:32:06 +0100

    Argument for checkbox in demadns

--- code/care4care/branch/templatetags/jobs_extra.py
+++ code/care4care/branch/templatetags/jobs_extra.py
@@ -6,5 +6,5 @@ def show_offers(request, offers, show_branch=True):
     return locals()
 
 @register.inclusion_tag('templatetags/demands.html')
-def show_demands(request, demands, show_branch=True):
+def show_demands(request, demands, show_branch=True, check_progress=True):
     return locals()
